1a78fe164288b94a805ad3c2f11d167c770321d9
David Payne <4246575+davidwrpayne@users.noreply.github.com>
2022-03-29 11:09:45 -0700

    Initial file repository implementation of storage

--- repository/file_repository.go
+++ repository/file_repository.go
@@ -1,16 +1,39 @@
 package repository
 
+import (
+	"github.com/davidwrpayne/wellmetersystem/schema"
+)
 
 type FileRepository struct {
 	Path string
 }
-
-
 
 func NewFileRepository(path string) *FileRepository {
 	return &FileRepository{
 		Path: path,
 	}
 }
+
+var _ Storage = (*FileRepository)(nil)
+
+func (f FileRepository) Load(uuid string) (*schema.Measurement, error) {
+	panic("implement me")
+}
+
+func (f FileRepository) FetchUnreported() ([]*schema.Measurement, error) {
+	panic("implement me")
+}
+
+func (f FileRepository) Store(measurement *schema.Measurement) error {
+	panic("implement me")
+}
+
+func (f FileRepository) MarkReported(id string) error {
+	panic("implement me")
+}
+
+
+
+
 
 
