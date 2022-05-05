8b2346018148d7ae2fd93fe1e9c6807463550bb9
David Payne <david.payne@chime.com>
2022-05-05 07:11:42 -0700

    use RPIO library to interact with the gpio pins on the raspberry pi

--- sensor/rpi_distance_sensor.go
+++ sensor/rpi_distance_sensor.go
@@ -1,5 +1,11 @@
 package sensor
 
+import (
+	"errors"
+	"github.com/stianeikeland/go-rpio"
+	"time"
+)
+
 type RPIDistanceSensor struct {
 	trigger     int
 	echo        int
@@ -22,7 +28,60 @@ func NewRPIDistanceSensor(triggerGPIOPin, echoGPIOPin, mode int, warnings bool) 
 	}
 }
 
-func (R RPIDistanceSensor) MeasureCM() (float64, error) {
-	
-	panic("implement me")
+const triggerTimeMicro = 10 * time.Microsecond
+const maxTimeSeconds = 0.015
+const speedOfSoundCmPerS = 34300.0
+
+//
+//func roundTripDistanceCmToS(distance float64) float64 {
+//	return (distance * 2.0) / speedOfSoundCmPerS
+//}
+
+func (s RPIDistanceSensor) MeasureCM() (float64, error) {
+
+	err := rpio.Open()
+	if err != nil {
+		return 0.0, err
+	}
+	defer rpio.Close() // guarantees we unmap the memory for gpio
+
+	// setup pins
+	echoPin := rpio.Pin(s.echo)
+	triggerPin := rpio.Pin(s.trigger)
+	echoPin.Input()
+	echoPin.PullUp() // Online it looks like people use PUllUP? why not pulldown
+	triggerPin.Output()
+
+	// Set trigger pin high for at least 10 Microseconds
+	triggerPin.High()
+	var echoStartTime time.Time
+	var echoStopTime time.Time
+	var startTime time.Time = time.Now() // used for timeouts
+
+	// pulse trigger
+	time.Sleep(triggerTimeMicro)
+	echoStartTime = time.Now() // set startTime in case of super fast response
+	triggerPin.Low()
+
+	// wait for start of echo response / wait for pin to go high
+	for echoPin.Read() == rpio.Low && time.Since(startTime).Seconds() <= maxTimeSeconds { // wait while echo pin is
+		echoStartTime = time.Now()
+	}
+
+	if time.Since(startTime).Seconds() > maxTimeSeconds {
+		return 0.0, errors.New("timeout reached while waiting for echo start")
+	}
+
+	// Wait for echo end
+	echoStopTime = time.Now()
+	for echoPin.Read() == rpio.High && time.Since(startTime).Seconds() <= maxTimeSeconds {
+		echoStopTime = time.Now() // continue overwriting stop time
+	}
+
+	elapsedSeconds := echoStopTime.Sub(echoStartTime).Seconds()
+	if elapsedSeconds <= maxTimeSeconds {
+		return float64(elapsedSeconds*speedOfSoundCmPerS) / 2.0, nil
+	} else {
+		return 0.0, errors.New("timeout reached while waiting for echo end")
+	}
 }
